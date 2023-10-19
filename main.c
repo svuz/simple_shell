aeb50799c8413f646ee94f3f80a11670b6b4d079
svuz <smblackeye@gmail.com>
2023-10-19 04:06:50 +0100

    ss

--- main.c
+++ main.c
@@ -6,42 +6,45 @@
 
 #define MAX_COMMAND_LENGTH 100
 
-int run(void){
+int run(void)
+{
     char input[MAX_COMMAND_LENGTH];
-    while (1) {
+    while (1)
+    {
         printf("$");
         
         // Read a command from the user
-        if (fgets(input, sizeof(input), stdin) == NULL) {
+        
+        if (fgets(input, sizeof(input), stdin) == NULL)
+        {
             perror("Failed to read input");
             exit;
         }
-        
-        // Remove the newline character
         size_t input_length = strlen(input);
         if (input_length > 0 && input[input_length - 1] == '\n') {
             input[input_length - 1] = '\0';
         }
 
-        
-        // Create a child process to execute the command
         pid_t pid = fork();
         
         if (pid < 0) {
             perror("Fork failed");
             exit(1);
-        } else if (pid == 0) {  // Child process
-            // Execute the command
-            if (execve(input, (char *)NULL) == -1) {
+        } else if (pid == 0)
+        {
+            if (execve(input, (char *)NULL) == -1)
+            {
                 perror("Command execution failed");
                 exit(1);
             }
-        } else {  // Parent process
-            // Wait for the child process to finish
+        }
+        else
+        { 
             int status;
             wait(&status);
             
-            if (WIFEXITED(status)) {
+            if (WIFEXITED(status))
+            {
                 int exit_status = WEXITSTATUS(status);
                 if (exit_status != 0) {
                     fprintf(stderr, "Command failed with exit status %d\n", exit_status);
