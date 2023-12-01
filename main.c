0e895f17e13381883332d7655d65cb262fa66be6
Saad <smblackeye@gmail.com>
2023-12-01 00:57:27 -0800

    comit

--- main.c
+++ main.c
@@ -1,200 +1,44 @@
-#include "main.h"
-/**
- * exec_command - creates a child process and execute command
- * @exit_status: exit status
- * @fullpath: executable command
- * @tokens: array of arguments passed
- * Return: void
- */
-int exec_command(int *exit_status, char *fullpath, char *tokens[])
-{
-	pid_t child;
-	int status;
-
-	child = fork();
-	if (child == -1)
-	{
-		perror("fork");
-		return (1);
-	} /* end if */
-	if (child == 0)
-	{
-		execve(fullpath, tokens, environ);
-		*exit_status = 2;
-		perror("execve");
-		exit(*exit_status);
-	} /* end if */
-	else
-	{
-		wait(&status);
-		if (WIFEXITED(status))
-		{
-			*exit_status = WEXITSTATUS(status);
-		} /* end if */
-	} /* end else */
-	if (fullpath != tokens[0])
-		free(fullpath);
-	return (0);
-} /* end function */
+#include "shell.h"
 
 /**
- * handle_prompt - prints prompt and gets input
- * @length: length of input
- * @line: user input stored
- * Return: void
- */
-int handle_prompt(size_t *length, char **line)
-{
-	ssize_t bytes_read;
-	char newline = '\n';
-	int is_interactive = isatty(STDIN_FILENO);
-
-	if (is_interactive)
-	{
-		write(STDOUT_FILENO, "$ ", 2);
-	} /* end if */
-	bytes_read = getline(line, length, stdin);
-	if (bytes_read == -1)
-	{
-		if (is_interactive)
-		{
-			write(STDOUT_FILENO, &newline, 1);
-		} /* end if */
-	return (1);
-	} /* end if */
-	return (0);
-} /* end function */
-
-/**
- * check_builtins - check for builtins
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
  *
- * @cnt: number of arguments passed in shell
- * @tokens: list of shell arguments
- * @argv: command line arguments array
- * @exit_status: exit status
- * Return: void
+ * Return: 0 on success, 1 on error
  */
-int check_builtins(int cnt, char **tokens, int *exit_status, char **argv)
+int main(int ac, char **av)
 {
-	char **env = environ;
-	char newline = '\n';
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	if (cnt < 1)
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
+	if (ac == 2)
 	{
-		return (2);
-	}
-	if (strcmp(tokens[0], "exit") == 0)
-	{
-		if (tokens[1] != NULL)
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
 		{
-			if (atoi(tokens[1]))
-				if (atoi(tokens[1]) >= 0)
-					*exit_status = _atoi(tokens[1]);
-				else
-					error_message(tokens, argv, exit_status);
-			else
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
 			{
-				error_message(tokens, argv, exit_status);
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
 			}
+			return (EXIT_FAILURE);
 		}
-		return (1);
+		info->readfd = fd;
 	}
-	if (strcmp(tokens[0], "env") == 0)
-	{
-		env = environ;
-		while (*env != NULL)
-		{
-			write(STDOUT_FILENO, *env, strlen(*env));
-			write(STDOUT_FILENO, &newline, 1);
-			env++;
-		}
-		return (2);
-	}
-	if (strcmp(tokens[0], "unsetenv") == 0)
-	{
-		_unsetenv(tokens[1]);
-		return (2);
-	}
-	return (0);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
-
-
-
-/**
- * _ch - checks for executable file errors
- * @argv: command line argument list
- * @tokens: array of user input
- * @fullpath: full path of command inputed
- * @exit_status: exit status
- * Return: void
- */
-int _ch(char **argv, char **tokens, char **fullpath, int *exit_status)
-{
-	char newline = '\n';
-	char *path = _getenv("PATH");
-
-	if (access(tokens[0], X_OK) == -1)
-	{
-		*fullpath = _which(tokens[0], path);
-		if (access(*fullpath, X_OK) == -1)
-		{
-			write(STDERR_FILENO, argv[0], strlen(argv[0]));
-			write(STDERR_FILENO, ": 1: ", 5);
-			write(STDERR_FILENO, tokens[0], strlen(tokens[0]));
-			write(STDERR_FILENO, ": not found", 11);
-			write(STDERR_FILENO, &newline, 1);
-			*exit_status = 127;
-			return (2);
-		} /* end if */
-	} /* end if */
-
-	return (0);
-}
-/**
- * main - simple shell
- * @argv: argument list
- * @argc: argument count
- * Return: exit status
- */
-int main(int argc __attribute__((unused)), char **argv)
-{
-	char *line = NULL;
-	size_t length = 0;
-	char *tokens[buffer];
-	int cnt, result;
-	char *fullpath;
-	int exit_status = 0;
-
-	while (1)
-	{
-		result = handle_prompt(&length, &line);
-		if (result == 1)
-			break; /* end  if */
-		cnt = 0;
-		tokens[cnt] = strtok(line, " \t\n");
-		while (tokens[cnt] != NULL)
-		{
-			cnt++;
-			tokens[cnt] = strtok(NULL, " \t\n");
-		} /* end while */
-		tokens[cnt] = NULL;
-		fullpath = tokens[0];
-		result = check_builtins(cnt, tokens, &exit_status, argv);
-		if (result == 1)
-			break; /* end if */
-		else if (result == 2)
-			continue; /* end else if */
-		result = _ch(argv, tokens, &fullpath, &exit_status);
-		if (result == 1)
-			break;/* end if */
-		else if (result == 2)
-			continue; /* else if */
-		result = exec_command(&exit_status, fullpath, tokens);
-		if (result == 1)
-			break; /* end if */
-		else if (result == 2)
-			continue; /*end if*/
-	} /* End while */
-	free(line);
-	return (exit_status);
-} /* end function */
